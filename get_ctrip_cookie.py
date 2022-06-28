7b73a1a1deba2694eed7b64dc6927d9cca0e0a69
Cjingger <53988000+Cjingger@users.noreply.github.com>
2022-06-28 22:27:59 +0800

    获取ctrip的cookie

--- get_ctrip_cookie.py
+++ get_ctrip_cookie.py
@@ -1,43 +1,96 @@
 # -*-encoding: utf-8 -*-
 # !/bin/usr/python3
-from bshead import create_bs_driver
-import time, random, datetime, os
 
+# !/usr/bin/python3
+# -*- encoding: utf-8 -*-
+import mitmproxy.http
+from mitmproxy import ctx
+import traceback, os
+from mitmdump import DumpMaster, Options
 from redis_util import RedisUtil
+import datetime
 
 redisUtil = RedisUtil()
 
 
-def get_ctrip_cookie(from_city_code: str, to_city_code: str, off_date: str):
-    _url = f"https://m.ctrip.com/html5/flight/swift/domestic/{from_city_code}/{to_city_code}/{off_date}?dfilter="
-    browser = create_bs_driver()
-    browser.get(_url)
-    print("get_url", _url)
-    time.sleep(random.choice([i for i in range(3, 5)]))
-    # dictCookies = browser.get_cookies()
-    # print(dictCookies)
-    # _cookies = []
-    # for _cookie in dictCookies:
-    #     for key, value in _cookie.items():
-    #         k = _cookie.get('name')
-    #         v = _cookie.get('value')
-    #         __cookie = f"{k}={v}"
-    #         _cookies.append(__cookie)
-    #         break
-    # cookies = "; ".join(_cookies)
-    # redisUtil.set(f"ctrip:{off_date}-cookie", "cookies", cookies)
-    browser.quit()
-    # return cookies
+class catch_cookies(object):
+    '''
+    利用mitmproxy模拟一个完整的HTTP通信周期获取\修改数据
+    '''
+
+    def __init__(self):
+        self.cookie = ""
+        self.num = 0
+        # self.filter_host, self.url_path 做加密处理
+        self.filter_host = "m.ctrip.com"
+        # self.url_path = "https://m.ctrip.com/restapi/soa2/14022/flightListSearch"
+        # self.url_path = "https://m.ctrip.com/restapi/soa2/14488/flightList"
+        self.url_path = "https://m.ctrip.com/html5/flight/swift/domestic"
+        # self.url_path = "m.ctrip.com/html5/flight/pages/first"
+
+    # 与服务器建立代理连接,仅仅是client与proxy连接,不会触发request,response以及其他http事件
+    def http_conn(self, flow: mitmproxy.http.HTTPFlow):
+        pass
+
+    # 来自client的 HTTP 请求的头部被成功读取, body还是空的
+    def request_headers(self, flow: mitmproxy.http.HTTPFlow):
+        pass
+
+    # 来自client的 HTTP 请求被成功完整读取(包括请求头cookie以及body)
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        self.num += 1
+        if self.num > 2:
+            pass
+        if (flow.request.host == self.filter_host) and (self.url_path in str(flow.request.url)):
+            ctx.log.info(u"处理第 %d 个请求" % self.num)
+            print("处理第 %d 个请求" % self.num)
+            try:
+                self.cookie = ""
+                for key, value in flow.request.cookies.items():
+                    # if "wengine_vpn_ticket" not in key:
+                    #     pass
+                    # else:
+                    coo = "{}={}; ".format(key, value)
+                    self.cookie += coo
+                    # self.cookie += flow.request.cookies[i]
+                self.cookie = str(self.cookie).strip('; ')
+                print("cookie", self.cookie)
+                now_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+                redisUtil.set(f"ctrip:{now_date}-cookie", "cookies", self.cookie)
+                redisUtil.set_expire(f'ctrip:{now_date}-cookie', 60 * 60)
+                print("***cookie捕获成功***")
+                # 将cookie写进文本里,再写一个程序读取,直接在此模块导入redis会报没有redis模块(未解决)
+                with open(r'./cookies.txt', 'w', encoding="utf-8") as file:
+                    file.write(self.cookie)
+                    print("cookie写入成功\033[0m")
+            except:
+                print(traceback.print_exc())
+
+        else:
+            return
+
+    # 来自server的 HTTP的响应头部被成功读取, body还是空的
+    def response_headers(self, flow: mitmproxy.http.HTTPFlow):
+        pass
+
+    # 来自server的 HTTP 响应被成功完整读取
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        pass
+
+    # 处理响应异常, HTTP错误
+    def error(self, flow: mitmproxy.http.HTTPFlow):
+        pass
+
+
+addons = [
+    catch_cookies()
+]
 
 
 if __name__ == '__main__':
-    now_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
-    __now_day = datetime.datetime.strptime(now_date, "%Y-%m-%d")
-    off_date = __now_day + datetime.timedelta(days=random.choice([j for j in range(10)]))
-    __off_date = datetime.datetime.strftime(off_date, "%Y-%m-%d")
-    path = os.path.abspath(".") + os.sep + 'city_tw_data_3U.txt'
-    with open(path, "r+", encoding="utf-8") as f:
-        lines = f.readlines()
-        line = eval(lines[random.choice([i for i in range(1, 1010)])])
-        from_city_code, to_city_code, off_date = line['from_city_code'], line['to_city_code'], __off_date
-    get_ctrip_cookie(from_city_code, to_city_code, off_date)
+    # from mitmproxy.tools.main import mitmweb
+    #
+    # mitmweb()
+    opts = Options(listen_host='0.0.0.0', listen_port=8080, scripts=__file__)
+    m = DumpMaster(opts)
+    m.run()
